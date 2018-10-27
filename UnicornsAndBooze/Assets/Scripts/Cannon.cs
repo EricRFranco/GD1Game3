ddb6102a1e777d6e152e36db228190273a919d7b
Eric Franco <france2@rpi.edu>
2018-10-27 14:36:31 -0400

    Projectile spawns on mouse click

---
+++ UnicornsAndBooze/Assets/Scripts/Cannon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject projectilePrefab;
+    [SerializeField]
+    private Vector3 spawnOffset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+            Vector3 spawnPoint = transform.position + spawnOffset;
+            GameObject projectile = (GameObject)Instantiate(projectilePrefab, spawnPoint, transform.rotation);
+        }
+	}
+}
