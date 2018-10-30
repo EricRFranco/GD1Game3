a73384b5f3ed1353a87e5f321fabf2f6fed29312
Jared Okun <okunj20@gmail.com>
2018-10-30 17:36:50 -0400

    have grid to track on

---
+++ UnicornsAndBooze/Assets/Scripts/GenerateLevel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour {
+	public TextAsset level;
+	public GameObject wallPrefab;
+
+	public GameObject floorPrefab;
+
+	public Vector3 levelCenter;
+
+	string[] levelRows;
+
+	LevelManagerScript levelManager;
+	// Use this for initialization
+	void Awake () {
+		levelManager = GetComponent<LevelManagerScript> ();
+		levelRows = level.text.Split(new char[]{'\n'});
+		Bounds wallBounds = wallPrefab.GetComponent<MeshFilter> ().sharedMesh.bounds;
+		float wallLengthInMeters = wallBounds.extents.x * 2f;
+		float wallHalfHeightInMeters = wallBounds.extents.y;
+		float wallWidthInMeters = wallBounds.extents.z * 2f;
+		Bounds floorBounds = floorPrefab.GetComponent<MeshFilter> ().sharedMesh.bounds;
+		float floorLengthInMeters = floorBounds.extents.x * 2f;
+		float floorWidthInMeters = floorBounds.extents.z * 2f;
+
+		GameObject floor = Instantiate (floorPrefab);
+		floor.transform.position = levelCenter;
+
+		float zOffset = floorWidthInMeters / (float)(levelRows.Length - 1);
+
+		levelManager.levelLayout = new bool[levelRows.Length - 1][];
+
+		Vector3 topLeftCorner = new Vector3 (levelCenter.x - (floorLengthInMeters / 2f), levelCenter.y + wallHalfHeightInMeters, levelCenter.z - (floorWidthInMeters / 2));
+
+		for (int i = 0; i < levelRows.Length - 1; ++i) {
+			levelRows [i] = levelRows [i].Trim ();
+			float xOffset = floorLengthInMeters / (float)(levelRows [i].Length);
+
+			levelManager.levelLayout [i] = new bool[levelRows [i].Length];
+			for(int j = 0; j < levelRows[i].Length; ++j) {
+
+				Vector3 placePosition = new Vector3 (topLeftCorner.x + j * xOffset + wallLengthInMeters / 2f, 
+					                        topLeftCorner.y, topLeftCorner.z + i * zOffset + wallWidthInMeters / 2f);
+
+				if (levelRows [i] [j] == 'W') {
+					GameObject wall = Instantiate (wallPrefab, floor.transform);
+					wall.transform.position = placePosition;
+					levelManager.levelLayout [i] [j] = true;
+				} else {
+					
+					levelManager.levelLayout [i] [j] = false;
+				}
+					
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnicornsAndBooze/Assets/Scripts/LevelManagerScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph {
+	Hashtable nodeToNodesGraph;
+	public Graph() {
+		nodeToNodesGraph = new Hashtable ();
+	}
+
+	public void AddNode(Vector3 nodePosition, List<Vector3> connectedNodes){
+		nodeToNodesGraph.Add (nodePosition, connectedNodes);
+	}
+
+	public List<Vector3> GetConnectedNodes(Vector3 nodePosition){
+		return (List<Vector3>)nodeToNodesGraph[nodePosition];
+	}
+}
+
+public class LevelManagerScript : MonoBehaviour {
+
+	[System.NonSerialized]
+	public Graph graph;
+
+	[System.NonSerialized]
+	public bool[][] levelLayout;
+
+	// Use this for initialization
+	void Start () {
+		print (levelLayout);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void UpdateGraph(){
+		/*List<Vector3> connectedPositions = new List<Vector3> ();
+		bool up = false;
+		bool down = false;
+		bool right = false;
+		bool left = true;
+		if (i > 0 && levelRows [i - 1] [j] != 'W') {
+			connectedPositions.Add (new Vector3 (placePosition.x, placePosition.y, placePosition.z - zOffset));
+			up = true;
+		}
+		if (i < levelRows.Length - 2 && levelRows [i + 1] [j] != 'W') {
+			connectedPositions.Add (new Vector3 (placePosition.x, placePosition.y, placePosition.z + zOffset));
+			down = true;
+		}
+		if (j > 0 && levelRows [i] [j - 1] != 'W') {
+			connectedPositions.Add (new Vector3 (placePosition.x - xOffset, placePosition.y, placePosition.z));
+			left = true;
+		}
+		if (j < levelRows[i].Length - 1 && levelRows [i] [j + 1] != 'W') {
+			connectedPositions.Add (new Vector3 (placePosition.x + xOffset, placePosition.y, placePosition.z));
+			right = true;
+		}
+
+		if (up && left && levelRows [i - 1] [j - 1] != 'W') {
+
+		}*/
+	}
+}
