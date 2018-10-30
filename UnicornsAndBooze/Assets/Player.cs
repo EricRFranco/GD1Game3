da92450958faf36f6287d07b55c5189b5a6ee896
Eric Franco <france2@rpi.edu>
2018-10-30 17:25:50 -0400

    Back and forth movement

---
+++ UnicornsAndBooze/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(transform.position);
+		if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += (transform.forward * speed);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            transform.position -= (transform.forward * speed);
+        }
+	}
+}
