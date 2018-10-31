0aa95b977098025dd6b10c887c18806a35217c95
Jared Okun <okunj20@gmail.com>
2018-10-31 16:46:13 -0400

    visionFan added

--- UnicornsAndBooze/Assets/Scripts/PathfindingScript.cs
+++ UnicornsAndBooze/Assets/Scripts/PathfindingScript.cs
@@ -21,10 +21,19 @@ public class PathfindingScript : MonoBehaviour {
 	int currentIndexOnPath;
 	Rigidbody rbody;
 
+	GameObject visionCone;
+	public Material visionMaterial;
+	public float rayCastRange;
+	public float rayCastTotalAngle;
+	public int amountOfRayCasts;
 	// Use this for initialization
 	void Start () {
         levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManagerScript>();
 		rbody = GetComponent<Rigidbody> ();
+		visionCone = new GameObject("vision", typeof(MeshFilter), typeof(MeshRenderer));
+		visionCone.transform.position = Vector3.zero;
+
+		visionCone.GetComponent<MeshRenderer> ().material = visionMaterial;
 	}
 	
 	// Update is called once per frame
@@ -47,11 +56,14 @@ public class PathfindingScript : MonoBehaviour {
 			if (rbody.velocity.magnitude > maxVelocity) {
 				rbody.velocity = rbody.velocity.normalized * maxVelocity;
 			}
+			float angle = Mathf.Atan2 (rbody.velocity.x, rbody.velocity.z);
+			transform.eulerAngles = new Vector3 (0, Mathf.Rad2Deg * angle, 0);
+
 		} else {
 			rbody.velocity = Vector3.zero;
 			rbody.angularVelocity = Vector3.zero;
 		}
-		
+		UpdateVisionCone ();
 	}
 	
 
@@ -79,6 +91,44 @@ public class PathfindingScript : MonoBehaviour {
 			linearAcceleration += DynamicArrive (position, path [currentIndexOnPath], currentVelocity);
 		}
 		return linearAcceleration;
+	}
+
+	void UpdateVisionCone(){
+		
+		float currentAngle = transform.eulerAngles.y;
+		float minAngle = currentAngle - rayCastTotalAngle / 2f;
+
+		Vector3[] vertices = new Vector3[amountOfRayCasts + 2];
+		vertices [0] = transform.position;
+		Vector2[] uvs = new Vector2[amountOfRayCasts + 2];
+		uvs [0] = Vector2.zero;
+		int[] triangles = new int[(amountOfRayCasts) * 3];
+		for (int i = 0; i <= amountOfRayCasts; ++i) {
+			
+			float angle = Mathf.Deg2Rad * (minAngle + i * rayCastTotalAngle / (float)(amountOfRayCasts));
+			Vector3 dir = new Vector3 (Mathf.Sin(angle), 0f, Mathf.Cos(angle));
+			RaycastHit raycastHitData; 
+			bool hit = Physics.Raycast (transform.position, dir, out raycastHitData, rayCastRange);
+
+			Debug.DrawRay (transform.position, (hit ? raycastHitData.distance : rayCastRange) * dir);
+
+			vertices [i + 1] = transform.position + (hit ? raycastHitData.distance : rayCastRange) * dir;
+			uvs [i + 1] = Vector2.zero;
+			if (i != amountOfRayCasts) {
+				triangles [i * 3] = 0;
+				triangles [i * 3 + 1] = i + 1;
+				triangles [i * 3 + 2] = i + 2;
+			}
+		}
+
+		Mesh mesh = new Mesh ();
+		mesh.vertices = vertices;
+		mesh.uv = uvs;
+		mesh.triangles = triangles;
+
+		visionCone.GetComponent<MeshFilter> ().mesh = mesh;
+
+
 	}
 
 }
